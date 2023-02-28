840c5cc8c37e20e7419e29057db1abe14a73e69c
BhattSoham <98672977+BhattSoham@users.noreply.github.com>
2023-02-27 21:36:41 -0500

    Update Top.vhd

--- Top.vhd
+++ Top.vhd
@@ -34,17 +34,17 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity Top is
  Port (
   inst: in std_logic_vector(31 downto 0);
-  cont: out std_logic_vector(31 downto 0);
-  addr : out std_logic_vector(11 downto 0);
   write_en: in std_logic;
   clock: in std_logic;
+ flush : in std_logic;
+ cont: out std_logic_vector(31 downto 0);
+ addr: out std_logic_vector(11 downto 0);
   x_in: out STD_LOGIC_VECTOR(31 downto 0);
-  y_in : out STD_LOGIC_VECTOR(31 downto 0);
-    h : out STD_LOGIC_VECTOR(31 downto 0);
-  p_in : out STD_LOGIC_VECTOR(31 downto 0);
-  p1_in : out STD_LOGIC_VECTOR(31 downto 0);
-  c_in : out STD_LOGIC_VECTOR(31 downto 0)
- 
+           y_in : out STD_LOGIC_VECTOR(31 downto 0);
+           h : out STD_LOGIC_VECTOR(31 downto 0);
+           p_in : out STD_LOGIC_VECTOR(31 downto 0);
+           p1_in : out STD_LOGIC_VECTOR(31 downto 0);
+           c_in : out STD_LOGIC_VECTOR(31 downto 0)
  );
 end Top;
 
@@ -79,19 +79,28 @@ Port (
 component Control is
 Port (
 inst: in std_logic_vector(31 downto 0);
-addr: out std_logic_vector(11 downto 0)
-
+addr: out std_logic_vector(11 downto 0);
+flush: in std_logic
  );
 end component;
 
  
 
-
+signal addr1: std_logic_vector(11 downto 0);
 signal wdata:  std_logic_vector(31 downto 0);
-signal  clk:  std_logic := '1';
+signal  clk:  std_logic;
 signal  rs :  std_logic_vector(4 downto 0);
 signal  regwr :  std_logic := '1';
-signal  flush :  std_logic;
+--signal   x_in:  STD_LOGIC_VECTOR(31 downto 0);
+--signal  y_in :  STD_LOGIC_VECTOR(31 downto 0);
+--signal    h :  STD_LOGIC_VECTOR(31 downto 0);
+--signal  p_in :  STD_LOGIC_VECTOR(31 downto 0);
+--signal  p1_in :  STD_LOGIC_VECTOR(31 downto 0);
+--signal   c_in :  STD_LOGIC_VECTOR(31 downto 0);
+
+
+
+--signal  flush :  std_logic;
 --signal inst:  std_logic_vector(31 downto 0);
 
 begin
@@ -99,7 +108,7 @@ begin
 uut1: Mem port map (
 
 cont => wdata,
-addr => inst(31 downto 20),
+addr => addr1,
 write_en => write_en,
 clock => clock,
 x_in => x_in,
@@ -121,11 +130,11 @@ uut2: reg port map (
 
 uut3: Control port map (
 inst => inst,
-addr => addr
-
+addr => addr1,
+flush => flush
 );
-addr <= inst(31 downto 20);
 cont <= wdata;
-
+--addr1 <= inst(31 downto 20);
+addr <= addr1;
 
 end Behavioral;
